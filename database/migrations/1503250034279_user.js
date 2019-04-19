05ecc6e0b489a8650d358752da9f2abca82e3a40
Pedro Rogério <pinceladasdaweb@hotmail.com>
2019-04-19 20:56:10 -0300

    Added user migrations

--- database/migrations/1503250034279_user.js
+++ database/migrations/1503250034279_user.js
@@ -7,9 +7,11 @@ class UserSchema extends Schema {
   up () {
     this.create('users', (table) => {
       table.increments()
-      table.string('username', 80).notNullable().unique()
+      table.string('name', 80).notNullable()
+      table.string('surname', 200).notNullable()
       table.string('email', 254).notNullable().unique()
       table.string('password', 60).notNullable()
+      table.integer('image_id').unsigned()
       table.timestamps()
     })
   }
