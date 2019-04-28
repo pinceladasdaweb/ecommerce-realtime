66b1b8d97573f4d271a88fb0f725ce455eca3462
Pedro Rogério <pinceladasdaweb@hotmail.com>
2019-04-27 21:35:53 -0300

    Applying Validations

--- start/routes/admin.js
+++ start/routes/admin.js
@@ -4,14 +4,22 @@
 const Route = use('Route')
 
 Route.group(() => {
-  Route.resource('categories', 'CategoryController').apiOnly()
+  Route.resource('categories', 'CategoryController').apiOnly().validator(new Map([
+    [['categories.store'], ['Admin/StoreCategory']],
+    [['categories.update'], ['Admin/StoreCategory']]
+  ]))
   Route.resource('products', 'ProductController').apiOnly()
   Route.resource('coupons', 'CouponController').apiOnly()
   Route.post('orders/:id/discount', 'OrderController.applyDiscount')
   Route.delete('orders/:id/discount', 'OrderController.removeDiscount')
-  Route.resource('orders', 'OrderController').apiOnly()
+  Route.resource('orders', 'OrderController').apiOnly().validator(new Map([
+    [['orders.store'], ['Admin/StoreOrder']]
+  ]))
   Route.resource('images', 'ImageController').apiOnly()
-  Route.resource('users', 'UserController').apiOnly()
+  Route.resource('users', 'UserController').apiOnly().validator(new Map([
+    [['users.store'], ['Admin/StoreUser']],
+    [['users.update'], ['Admin/StoreUser']]
+  ]))
 })
 .prefix('v1/admin')
 .namespace('Admin')
