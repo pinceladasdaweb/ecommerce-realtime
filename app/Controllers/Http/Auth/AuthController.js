e45d1ff1430670be162261a3e7d20cba5e075ada
Pedro Rogério <pinceladasdaweb@hotmail.com>
2019-04-28 17:09:39 -0300

    Added websockets

--- app/Controllers/Http/Auth/AuthController.js
+++ app/Controllers/Http/Auth/AuthController.js
@@ -3,6 +3,7 @@
 const Database = use('Database')
 const Role = use('Role')
 const User = use('App/Models/User')
+const Ws = use('Ws')
 
 class AuthController {
   async register ({ request, response }) {
@@ -15,6 +16,12 @@ class AuthController {
       
       await user.roles().attach([userRole.id], null, trx)
       await trx.commit()
+
+      const topic = Ws.getChannel('notifications').topic('notifications')
+
+      if (topic) {
+        topic.broadcast('new:user')
+      }
 
       return response.status(201).send({ data: user })
     } catch (error) {
