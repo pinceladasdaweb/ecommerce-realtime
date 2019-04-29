d3786f9d46809ba01c221f20ef9bed582fb25aa0
Pedro Rogério <pinceladasdaweb@hotmail.com>
2019-04-28 22:00:49 -0300

    Update routes

--- start/routes/client.js
+++ start/routes/client.js
@@ -9,7 +9,6 @@ Route.group(() => {
   Route.get('orders', 'OrderController.index').middleware(['auth'])
   Route.get('orders/:id', 'OrderController.show').middleware(['auth'])
   Route.post('orders', 'OrderController.store').middleware(['auth'])
-  Route.put('orders/:id', 'OrderController.update').middleware(['auth'])
 })
 .prefix('v1')
 .namespace('Client')
